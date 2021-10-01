acedbdc6dbe303ac17693e85cd72b2369e6a3cc6
Michael Elnajami <makmoud98@gmail.com>
2021-10-01 15:15:43 -0500

    fixed a typo

--- dep.require.build.js
+++ dep.require.build.js
@@ -1,6 +1,6 @@
 ({
-	namespace: 'dep', // We can name this to whatever, will allow us to do `dep.require` instead of `require`
+    namespace: 'dep', // We can name this to whatever, will allow us to do `dep.require` instead of `require`
     baseUrl: ".", // Just tells the script to look in the same folder
     name: "require.2.3.6.js", // The input require script (whatever version DEP currently uses)
-    out: "dep.require.2.3.6.js" // The output require script with out namespace changes
+    out: "dep.require.2.3.6.js" // The output require script with our namespace changes
 });
