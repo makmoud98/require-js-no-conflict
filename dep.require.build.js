1ea70c68d665f066748a8ed82f18e7811c2e6a21
Michael Elnajami <makmoud98@gmail.com>
2021-10-07 16:05:52 -0500

    added correct version of require script for DEP

--- dep.require.build.js
+++ dep.require.build.js
@@ -1,6 +1,6 @@
 ({
-	namespace: 'dep', // We can name this to whatever, will allow us to do `dep.require` instead of `require`
+	namespace: 'dep_require', // We can name this to whatever, will allow us to do `dep.require` instead of `require`
     baseUrl: ".", // Just tells the script to look in the same folder
-    name: "require.2.3.6.js", // The input require script (whatever version DEP currently uses)
-    out: "dep.require.2.3.6.js" // The output require script with out namespace changes
+    name: "require.2.1.17.min.js", // The input require script (whatever version DEP currently uses)
+    out: "dep.require.2.1.17.js" // The output require script with out namespace changes
 });
